454515ae526dd1d94660d04245efc374e4af5047
Ash Morrissey <a.morrisseyy@gmail.com>
2022-04-19 11:05:46 +0100

    DeckScript and game manager created, basic game layout done

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [Header ("Game Buttons")]
+    public Button dealBtn;
+    public Button hitBtn;
+    public Button standBtn;
+    public Button doubleBtn;
+    public Button betBtn;
+    void Start()
+    {
+        dealBtn.onClick.AddListener(() => DealClicked());
+        hitBtn.onClick.AddListener(() => HitClicked());
+        standBtn.onClick.AddListener(() => StandClicked());
+        //doubleBtn.onClick.AddListener(() => DoubleClicked());
+    }
+
+
+    void DealClicked()
+    {
+        
+    }
+
+    void HitClicked()
+    {
+        
+    }
+
+    void StandClicked()
+    {
+        
+    }
+
+    //void DoubleClicked(){}
+   
+}
